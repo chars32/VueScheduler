128ba9798e6f56f1ec99352fb90ef49e5b18393d
Carlos Garcia <chars24@gmail.com>
2017-07-07 21:21:05 -0500

    2. Estilos Header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,56 +1,17 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-    >
-      <v-list>
-        <v-list-tile 
-          router
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar fixed>
-      <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn 
-        icon
-        @click.native.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.native.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.native.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
+  <v-app light>
+
+    <v-toolbar fixed class="indigo indigo--text text--lighten-5">
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.native.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
+      <v-layout v-for="item in items" :key="item.id">
+        <v-btn class="indigo--text text--lighten-5" flat>
+        <v-icon class="mx-1 indigo--text text--lighten-5">{{item.icon}}</v-icon>
+        <span class="mx-1">{{item.name}}{{item.id}}</span>
       </v-btn>
+      </v-layout>
     </v-toolbar>
+
     <main>
       <v-container fluid>
         <v-slide-y-transition mode="out-in">
@@ -58,20 +19,7 @@
         </v-slide-y-transition>
       </v-container>
     </main>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+
     <v-footer :fixed="fixed">
       <span>&copy; 2017</span>
     </v-footer>
@@ -82,18 +30,21 @@
   export default {
     data () {
       return {
-        clipped: false,
-        drawer: true,
         fixed: false,
+        title: 'VueScheduler',
         items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+          {name: 'VIEW MEETUPS', icon:'date_range'},
+          {name: 'ORGANIZE MEETUP', icon:'room'},
+          {name: 'PROFILE', icon:'perm_identity'},
+          {name: 'LOGOUT', icon:'exit_to_app'}
+        ]
       }
     }
   }
 </script>
+
+<style>
+  #icon{
+    margin: 0, 5px;
+  }
+</style>
