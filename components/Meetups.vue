7616710ffb8229c4952c78d336b9ae70f3ddcb5f
Carlos Garcia <chars24@gmail.com>
2017-07-11 23:26:45 -0500

    :wrench: 8.7 Corrigiendo estilos en el Meetups, flex-wrap.

--- components/Meetups.vue
+++ components/Meetups.vue
@@ -1,24 +1,24 @@
 <template>
-  <v-layout class="cajas-cards">
-    <v-card v-for="(meetup, i) in meetups" :key='i' class="cards">
-      <v-card-media
-        :src= "meetup.src"
-        height="200px"
-      >
-      </v-card-media>
-      <v-card-title primary-title>
-        <div>
-          <div class="headline">Top western road trips</div>
-          <span class="grey--text">July 31, 2017, 5:30pm</span>
-        </div>
-      </v-card-title>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn router :to="'/meetup/details/'+meetup.id" icon class="boton-hover">
-          <v-icon class="icon-hover">{{ 'forward' }}</v-icon>
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+  <v-layout class="caja">
+    <v-card v-for="(meetup, i) in meetups" :key='i' class="cajita my-4">
+        <v-card-media
+          :src= "meetup.src"
+          height="200px"
+        >
+        </v-card-media>
+        <v-card-title primary-title>
+          <div> 
+            <div class="headline">{{ meetup.name}}</div>
+            <span class="grey--text">{{meetup.date}}</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn router :to="'/meetup/details/'+meetup.id" icon class="boton-hover">
+            <v-icon class="icon-hover">{{ 'forward' }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
   </v-layout>
 </template>
 
@@ -26,16 +26,22 @@
   import datos from '../store/bdprueba.js'
 
   export default {
-    props: ['meetups']
+    props: ['meetups'],
+    data() {
+      return {
+        show: false
+      }
+    }
   }
 </script>
 
 <style scoped>
-  .cajas-cards{
+  .caja{
+    display: flex;
+    flex-wrap: wrap;
     justify-content: space-between;
   }
-
-  .cards{
+  .cajita{
     width: 30%;
   }
 </style>
